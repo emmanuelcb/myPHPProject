230b4069f84d51b91cc77d56160a0fddfe5b31ed
emmanuelcb <emmanuel.cb@live.com.mx>
2016-06-24 15:00:28 -0500

    Updated index.php

--- index.php
+++ index.php
@@ -1,10 +1,24 @@
 <html>
- <head>
-  <title>PHP Test</title>
- </head>
- <body>
- <?php 
- 	echo '<p>Hello World</p>';
- ?> 
- </body>
+  <head>
+  	<title>PHP Test</title>
+  </head>
+  <body>
+    <?php
+    function pgConnectionStr(){
+  		$pgConnectionStr = "dbname=devpm0vpe3687n ";
+  		$pgConnectionStr += "host=ec2-54-243-42-108.compute-1.amazonaws.com ";
+  		$pgConnectionStr += "port=5432 ";
+  		$pgConnectionStr += "user=uwqryzqrrrnmwk ";
+  		$pgConnectionStr += "password=h-LZdtvOsqn2bIifjq32FbKDdr ";
+  		$pgConnectionStr += "sslmode=require";
+  		return $pgConnectionStr;
+    }
+	$db = pg_connect(pgConnectionStr());
+	if(!$db){
+    	echo "Database connection error.";
+        exit;
+    }
+    echo '<p>Hello World</p>';
+    ?>
+  </body>
 </html>
