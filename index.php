2d205927e649feded1eafa5e6bc384268a302b92
emmanuelcb <emmanuel.cb@live.com.mx>
2016-06-24 15:37:59 -0500

    Updated index.php

--- index.php
+++ index.php
@@ -15,6 +15,7 @@
         $strPgConnection .= 'user='.$strUser.' password='.$strPass.' sslmode='.strSSLMode;
       	return $strPgConnection;
     }
+	echo getStrPgConnection;
 	$db = pg_connect(getStrPgConnection());
 	if(!$db){
     	echo "Database connection error.";
