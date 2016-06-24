8a518cd2a250f09ccbee68b0646cc78907fce786
emmanuelcb <emmanuel.cb@live.com.mx>
2016-06-24 15:06:32 -0500

    Updated index.php

--- index.php
+++ index.php
@@ -4,14 +4,14 @@
   </head>
   <body>
     <?php
-  	echo '<p>'+pgConnectionStr()+'Hello World</p>';
+  	echo '<p>'.pgConnectionStr().'Hello World</p>';
     function pgConnectionStr(){
   		$pgConnectionStr = "dbname=devpm0vpe3687n ";
-  		$pgConnectionStr += "host=ec2-54-243-42-108.compute-1.amazonaws.com ";
-  		$pgConnectionStr += "port=5432 ";
-  		$pgConnectionStr += "user=uwqryzqrrrnmwk ";
-  		$pgConnectionStr += "password=h-LZdtvOsqn2bIifjq32FbKDdr ";
-  		$pgConnectionStr += "sslmode=require";
+  		$pgConnectionStr .= "host=ec2-54-243-42-108.compute-1.amazonaws.com ";
+  		$pgConnectionStr .= "port=5432 ";
+  		$pgConnectionStr .= "user=uwqryzqrrrnmwk ";
+  		$pgConnectionStr .= "password=h-LZdtvOsqn2bIifjq32FbKDdr ";
+  		$pgConnectionStr .= "sslmode=require";
   		return $pgConnectionStr;
     }
 	$db = pg_connect(pgConnectionStr());
